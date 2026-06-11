9f4260c2d7ebeca563a5817817fffae5dd835d28
Ksusha <ksusenkakorneva@gmail.com>
2026-06-11 23:39:02 +0300

    add reporter Docker commands

--- run.sh
+++ run.sh
@@ -15,11 +15,22 @@ case "$1" in
     python3 generator/generate.py local_data
     ;;
 
+  build_reporter)
+    docker build -t hw-reporter ./reporter
+    ;;
+
+  run_reporter)
+    mkdir -p data
+    docker run --rm -v "$(pwd)/data:/data" hw-reporter
+    ;;
+
   *)
     echo "Unknown command: $1"
     echo "Available commands:"
     echo "  build_generator"
     echo "  run_generator"
     echo "  create_local_data"
+    echo "  build_reporter"
+    echo "  run_reporter"
     ;;
 esac
