5306178cd166f78fdfed910b9d8fa447c6645aaa
Ksusha <ksusenkakorneva@gmail.com>
2026-06-11 23:19:55 +0300

    add local data generation command

--- run.sh
+++ run.sh
@@ -10,10 +10,16 @@ case "$1" in
     docker run --rm -v "$(pwd)/data:/data" hw-generator
     ;;
 
+  create_local_data)
+    mkdir -p local_data
+    python3 generator/generate.py local_data
+    ;;
+
   *)
     echo "Unknown command: $1"
     echo "Available commands:"
     echo "  build_generator"
     echo "  run_generator"
+    echo "  create_local_data"
     ;;
 esac
