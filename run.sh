5d96f2cf8b8cbe3a7bf58cd1bc20eb53b0613cbe
Ksusha <ksusenkakorneva@gmail.com>
2026-06-11 23:11:42 +0300

    add run generator command

--- run.sh
+++ run.sh
@@ -5,9 +5,15 @@ case "$1" in
     docker build -t hw-generator ./generator
     ;;
 
+  run_generator)
+    mkdir -p data
+    docker run --rm -v "$(pwd)/data:/data" hw-generator
+    ;;
+
   *)
     echo "Unknown command: $1"
     echo "Available commands:"
     echo "  build_generator"
+    echo "  run_generator"
     ;;
 esac
